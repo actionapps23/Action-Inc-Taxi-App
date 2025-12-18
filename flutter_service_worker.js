55730b6c0b3cb5b1cc53c6c6f222c045681b9ba4
Muneeb Masood <muneebned844@gmail.com>
2025-12-18 10:01:20 +0500

    Built web
    Branch: main
    Commit: 075743df22a3752b71cacf33bdc95a1a79352e6c (dirty)
    
    package:peanut 6.0.0

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -53,7 +53,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "abb36797a244f3c8e39626a92c1518aa
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "09823545ca2542d57cb3700892149eaf",
+"flutter_bootstrap.js": "033274d1109fd0d7c2ee674d081cd761",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
